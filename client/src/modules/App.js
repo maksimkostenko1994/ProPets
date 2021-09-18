b45fbf03cb09b2011e0ec5a464d470e6c35201af
Maksym Kostenko <kostenko.m.t@gmail.com>
2021-09-18 14:58:56 +0200

    Update

--- client/src/modules/App.js
+++ client/src/modules/App.js
@@ -1,17 +1,19 @@
 import MainPageSection from './components/mainPage/MainPageSection';
 import Button from "./components/button/Button.js";
+import "../sass/textBtn.scss"
 
 function App() {
     return (
         <>
-        <MainPageSection/>
-        <Button btnStyle='btnType2' btnIcon='paw' text='Publish'/>
-        <Button btnStyle='btnType2' btnIcon='paw' text='Submit' />
-        <Button btnStyle='btnType3' btnIcon='search' text='I lost my pet' />
-        <Button btnStyle='btnType4' btnIcon='paw' text='I found a pet' />
-        <Button btnStyle='btnType4' btnIcon='plus' text='Add new' />
-        <Button btnStyle='btnType4' btnIcon='save' text='Save changes' />
-        <Button btnStyle='btnType5' btnIcon='' text='Cancel' />
+            <MainPageSection/>
+            <Button btnStyle='btnType2' btnIcon='paw' text='Publish'/>
+            <Button btnStyle='btnType2' btnIcon='paw' text='Submit'/>
+            <Button btnStyle='btnType3' btnIcon='search' text='I lost my pet'/>
+            <Button btnStyle='btnType4' btnIcon='paw' text='I found a pet'/>
+            <Button btnStyle='btnType4' btnIcon='plus' text='Add new'/>
+            <Button btnStyle='btnType4' btnIcon='save' text='Save changes'/>
+            <Button btnStyle='btnType5' btnIcon='' text='Cancel'/>
+            <Button btnStyle='btnType4 testNavBtn' btnIcon='paw' text='I found a pet'/>
         </>
     );
 }
