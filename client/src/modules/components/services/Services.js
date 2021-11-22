db9639130033d39e26f510b137aaff63b07dadd2
Maksym Kostenko <kostenko.m.t@gmail.com>
2021-11-22 18:53:12 +0100

    add active class to pagination bar

--- client/src/modules/components/services/Services.js
+++ client/src/modules/components/services/Services.js
@@ -3,7 +3,7 @@ import HotelList from "./hotels/HotelList";
 import WalkingList from "./walking/WalkingList";
 import FosteringList from "./fostering/FosteringList";
 import VetHelpList from "./vet_help/VetHelpList";
-import {useParams} from "react-router-dom";
+import {NavLink, useParams} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {getServicesAction, serviceSelector} from "../../../store/service";
 import {paginationSelector, setCurrentPageAction} from "../../../store/pagination";
@@ -30,7 +30,13 @@ const Services = () => {
     return (
         <div>
             <div className="service-pagination">{pagesArr(pages).map(item =>
-                <span onClick={() => dispatch(setCurrentPageAction(item))} key={item}>{item}</span>
+                <a id={item} onClick={(event) => {
+                    dispatch(setCurrentPageAction(item))
+                    event.target.classList.add('service-active-link')
+                    Array.from(event.target.parentNode.children)
+                        .map(link => event.target.id !== link.id ? link.classList.remove("service-active-link") : ''
+                        )
+                }} key={item}>{item}</a>
             )}
             </div>
             {type === "Hotels" && <HotelList rows={rows}/>}
